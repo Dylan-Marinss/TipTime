d4b0a4822d53d8651f1dee9ae155f49c50166e3f
Dylan-Marinss <dylanmarins24@gmail.com>
2025-09-01 22:25:22 -0300

    Calculadora Gorjeta

---
+++ TipTime/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace TipTime
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void QuinzeporcBnt_Clicked(object sender, EventArgs e)
+        {
+            PorcentagemSlider.Value = 15;
+        }
+
+        private void VinteporcBnt_Clicked(object sender, EventArgs e)
+        {
+            PorcentagemSlider.Value = 20;
+        }
+
+        private void ArredondarParaMais_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ArrendodarParaMenos_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void PorcentagemSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            double valor = Convert.ToDouble(ValorTotalEntry.Text);
+            double porcetagem = e.NewValue;
+            double gorjeta = valor * (porcetagem / 100);
+            double valorFinal = valor + gorjeta;
+
+            ValorGorjetaLabel.Text = $"Gorjeta: {gorjeta:C}";
+            ValorTotalLabel.Text = $"Total com Gorgejta: {valorFinal}";
+        }
+    }
+
+}
